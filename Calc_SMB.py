0e0bce09a0b06838de5a4617586055fdfca210d6
Ella Gilbert <ellgil82@bas.ac.uk>
2020-08-03 12:20:51 +0100

    Create calc_SMB.py
    Outline of script routine

---
+++ Calc_SMB.py
@@ -0,0 +1,9 @@
+## Load in data files
+# Precipitation, evaporation, sublimation (?), runoff, snowmelt?, land-sea mask
+
+## Do some processing to unify units and wrangle data if necessary
+
+## Mask non-land points
+
+## Calculate SMB
+# SMB = precip - evap - sublim - runoff
